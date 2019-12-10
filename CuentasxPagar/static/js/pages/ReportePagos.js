9a731fa0efb09720c9f6a160075ed14369176d67
JesusFraga <56400298+JesusFraga@users.noreply.github.com>
2019-12-10 09:05:21 -0600

    front

--- CuentasxPagar/static/js/pages/ReportePagos.js
+++ CuentasxPagar/static/js/pages/ReportePagos.js
@@ -17,33 +17,40 @@ $(document).ready(function(){
     columnDefs: [
     {
       "targets": [0],
-      "width": "10px",
+      "width": "10%",
       "className": "dt-head-center dt-body-center"
     },
 
     {
       "targets": [1,2],
-      "width": "15px",
+      "width": "10%",
       "className": "dt-head-center dt-body-center"
     },
 
     {
-      "targets": [3,4],
-      "width": "12px",
+      "targets": [3],
+      "width": "5%",
+      "className": "dt-head-center dt-body-right"
+    },
+    {
+      "targets": [4],
+      "width": "9%",
       "className": "dt-head-center dt-body-right"
     },
 
     {
       "targets": 5,
-      "width": "3px",
+      "width": "2%",
+      "className": "dt-head-center dt-body-center",
       "mRender": function (data, type, full) {
-        return  '<button type ="button" class="btn btn-success btn-elevate btn-pill btn-sm" data-toggle="modal" data-target="#ModalComplemento" data-backdrop="static" data-keyboard="false"><i class="fas fa-upload"></i></button>';
+        return  '<button type ="button" class="btn btn-success btn-elevate btn-pill btn-sm" data-toggle="modal" data-target="#ModalComplementos" data-backdrop="static" data-keyboard="false"><i class="fas fa-upload"></i></button>';
       }
     },
 
     {
         "targets": 6,
-        "width": "3px",
+        "width": "2%",
+        "className": "dt-head-center dt-body-center",
         "mRender": function (data, type, full) {
           return  '<button type ="button" class="btnEliminarPago btn btn-danger btn-elevate btn-pill btn-sm"><i class="flaticon-delete"></i></button>';
         }
@@ -60,8 +67,18 @@ $(document).ready(function(){
     $(this).val(picker.startDate.format('MM/DD/YYYY') + ' - ' + picker.endDate.format('MM/DD/YYYY'));
   });
 
+  $('#ModalComplementos').on('hidden.bs.modal', function(){
+   var id = '#ComplementosPagos';
+   var verComp = '.uploaded-files-pagos';
+   KTUppyEvidencias.init(id, verComp)
+  });
 
 
+  KTUtil.ready(function() {
+    var id = '#ComplementosPagos';
+    var verComp = '.uploaded-files-pagos';
+    KTUppyEvidencias.init(id, verComp);
+  });
 
 
 
