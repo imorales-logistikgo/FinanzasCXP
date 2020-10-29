9ed3b65d326148dbce92f6930573d42ed6a86207
JesusFraga <56400298+JesusFraga@users.noreply.github.com>
2020-10-29 09:37:46 -0600

    manual cxp ruta

--- CuentasxPagar/Dashboard/urls.py
+++ CuentasxPagar/Dashboard/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.Indicadores, name="Indicadores"),
+    path('ManualPDF', views.ManualPDF, name='ManualPDF'),
 ]
--- CuentasxPagar/Dashboard/views.py
+++ CuentasxPagar/Dashboard/views.py
@@ -10,3 +10,11 @@ def Indicadores(request):
 	PE = View_PendientesEnviarCxP.objects.filter(FechaDescarga__year = today.year, FechaDescarga__month = today.month, FechaDescarga__day = today.day)
 	print(PE)
 	return render(request, 'Dashboard.html', {'today': today});
+
+
+def ManualPDF(request):
+    with open('static/pdf/Manual_cxp_proveedores.pdf', 'rb') as pdf:
+        response = HttpResponse(pdf.read(), content_type='application/pdf')
+        response['Content-Disposition'] = 'inline;filename=Manual_cxp_proveedores.pdf'
+        return response
+    pdf.closed
